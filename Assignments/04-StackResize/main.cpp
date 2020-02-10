17ea82bf71d232fdeb4c9c9baaa4c7d79dc1b37b
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2020-02-10 10:53:46 -0600

    Update main.cpp

--- Assignments/04-StackResize/main.cpp
+++ Assignments/04-StackResize/main.cpp
@@ -1,30 +1,30 @@
 /*
- 
-  ######    ###     #                 
-  #     #  #   #   ##                 
-  #     # #     # # #                 
-  ######  #     #   #                 
-  #       #     #   #                 
-  #        #   #    #                 
-  #         ###   #####               
-                                      
-   #####    ###     #    #####        
-  #     #  #   #   ##   #     #       
-        # #     # # #         #       
-   #####  #     #   #    #####        
-        # #     #   #         #       
-  #     #  #   #    #   #     #       
-   #####    ###   #####  #####        
-                                      
-        #                             
-        #  ####  #    # ######  ####  
-        # #    # ##   # #      #      
-        # #    # # #  # #####   ####  
-  #     # #    # #  # # #           # 
-  #     # #    # #   ## #      #    # 
-   #####   ####  #    # ######  ####  
-                                      
-                                      
+
+  ######    ###     #
+  #     #  #   #   ##
+  #     # #     # # #
+  ######  #     #   #
+  #       #     #   #
+  #        #   #    #
+  #         ###   #####
+
+   #####    ###     #    #####
+  #     #  #   #   ##   #     #
+        # #     # # #         #
+   #####  #     #   #    #####
+        # #     #   #         #
+  #     #  #   #    #   #     #
+   #####    ###   #####  #####
+
+        #
+        #  ####  #    # ######  ####
+        # #    # ##   # #      #
+        # #    # # #  # #####   ####
+  #     # #    # #  # # #           #
+  #     # #    # #   ## #      #    #
+   #####   ####  #    # ######  ####
+
+
 */
 ///////////////////////////////////////////////////////////////////////////////
 //
@@ -47,6 +47,326 @@
 //                   nums.dat    : contains data in integer format of unspecified amount
 ///////////////////////////////////////////////////////////////////////////////
 
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+/**
+ * ArrayStack
+ *
+ * Description:
+ *      Array based stack
+ *
+ * Public Methods:
+ *      ArrayStack(int s)               -parameterized constructor
+ *      bool Empty()                    -checks if list has elements
+ *      bool Full()                     -checks to see if list is full
+ *      void Print(ofstream& outfile)   -prints list elements in order
+ *      void Push(int x)                -pushes value onto the stack
+ *      void Pop()                      -pops value from the stack
+ *      void ContainerGrow()            -grows list if list is full when pushing
+ *      void ContainerShrink()          -shrinks list if not empty and capacity is less than half
+ *      int NumResized()                -returns number of times list was resized
+ *      int PrintTop()                  -prints top value
+ *      int PrintSize()                 -prints number of elements in list
+ *
+ * Usage:
+ *      - See main program
+ *
+ */
+class ArrayStack
+{
+private:
+    int* A;   // pointer to array of int's
+    int size; // current max stack size
+    int top;  // top of stack
+    int numResize; //used to keep track of how many times list is resized
+    int maxStackSize;
+public:
+    /**
+    * ArrayStack
+    *
+    * Description:
+    *      Constructor no params
+    *
+    * Params:
+    *     - None
+    *
+    * Returns:
+    *     - NULL
+    */
+    ArrayStack()
+    {
+        size = 10;          //inital size 10 for default
+        A = new int[size];  //allocate space for array
+        top = -1;           //empty means top is -1
+        numResize = 0;      //used to count number of times the stack is resized
+        maxStackSize = size;//used to remember what maximum size of stack overall is
+    }
+    /**
+    * ArrayStack
+    *
+    * Description:
+    *      Constructor size param
+    *
+    * Params:
+    *     - int size
+    *
+    * Returns:
+    *     - NULL
+    */
+    ArrayStack(int s)
+    {
+        size = s;           //inital size is passed by param
+        A = new int[s];     //allocate space for array
+        top = -1;
+        numResize = 0;
+        maxStackSize = size;
+    }
+    /**
+    * Public bool: Empty
+    *
+    * Description:
+    *      Checks to see if stack is empty
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      [bool] true = empty
+    */
+    bool Empty()
+    {
+        return (top <= -1);
+    }
+    /**
+    * Public bool: Full
+    *
+    * Description:
+    *      Checks to see if stack is full
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      [bool] true = full
+    */
+    bool Full()
+    {
+        return (top >= size - 1);
+    }
+    /**
+    * Public void: Print
+    *
+    * Description:
+    *      Prints stack to standard output
+    *
+    * Params:
+    *      ofstream: output file name
+    *
+    * Returns:
+    *      NULL
+    */
+    void Print(ofstream& outfile)
+    {
+        for (int i = 0; i <= top; i++)
+        {
+            outfile << A[i] << " ";
+        }
+        outfile << endl;
+    }
+    /**
+    * Public void: Push
+    *
+    * Description:
+    *      Adds an item to top of stack
+    *
+    * Params:
+    *      [int] : item to be added
+    *
+    * Returns:
+    *      NULL
+    */
+    void Push(int x)
+    {
+        if (Full())
+        {
+            ContainerGrow();
+        }
+        if (!Full())
+        {
+            A[++top] = x;
+        }
+    }
+    /**
+    * Public int: Pop
+    *
+    * Description:
+    *      Returns top value and removes it from stack
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      [int] top value if any
+    */
+    int Pop()
+    {
+        if (Empty())
+        {
+            return -200; //sentinel value 
+        }
+        else
+        {
+            if (top < (size / 2) && (size >= 20))
+                ContainerShrink(); //shrinks if capacity is half empty
+            return top--;   //returns top
+        }
+    }
+    /**
+    * Public void: ContainerGrow
+    *
+    * Description:
+    *      Resizes the container for the stack by multiplying its capacity by 1.75
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      NULL
+    */
+    void ContainerGrow()
+    {
+        int newSize = size * 1.75; // 1.75size of original
+        if (newSize > maxStackSize) //checks to see if max size needs to updated
+            maxStackSize = newSize;
+        int* B = new int[newSize]; // allocate new memory
+        for (int i = 0; i < size; i++)
+        { // copy values to new array
+            B[i] = A[i];
+        }
+        delete[] A;     // delete old array
+        size = newSize; // save new size
+        A = B;          // reset array pointer
+        numResize++;
+    }
+    /**
+    * Public void: ContainerShrink
+    *
+    * Description:
+    *      Resizes the container for the stack by multiplying its capacity by 0.50
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      NULL
+    */
+    void ContainerShrink()
+    {
+        int newSize = (size * 0.5) ;  // half size of original
+        int* B = new int[newSize]; // allocate new memory
+        for (int i = 0; i < top; i++)
+        { // copy values to new array
+            B[i] = A[i];
+        }
+        delete[] A;     // delete old array
+        size = newSize; // save new size
+        A = B;          // reset array pointer
+        numResize++;
+    }
+    /**
+    * Public int: NumResized
+    *
+    * Description:
+    *      getter for numresized
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      int: numResize
+    */
+    int NumResized()
+    {
+        return numResize;
+    }
+    /**
+    * Public int: PrintTop
+    *
+    * Description:
+    *      Prints index of top
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      [int]: Index of top
+    */
+    int PrintTop()
+    {
+        return top;
+    }
+    /**
+    * Public int: PrintSize
+    *
+    * Description:
+    *      returns number of times the stack was resized
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      [int] Value of Size
+    */
+    int PrintSize()
+    {
+        return size;
+    }
+    /**
+    * Public int: Max
+    *
+    * Description:
+    *      returns maximum size that the stack has been
+    *
+    * Params:
+    *      NULL
+    *
+    * Returns:
+    *      NULL
+    */
+    int Max()
+    {
+        return maxStackSize;
+    }
+};
+
+// MAIN DRIVER
+int main()
+{
+    ArrayStack stack;       //object initialization
+    int r = 0;              //int used to hold the read integer
+    ifstream infile;
+    infile.open("nums_test.dat");
+    ofstream outfile;
+    outfile.open("output.dat");
+    while (!infile.eof())
+    {
+        infile >> r;
+        if (r % 2 == 0)   //if even, push
+            stack.Push(r);
+        else            //if odd, pop
+            stack.Pop();
+    }
+    //Desired output
+    outfile << "######################################################################\n";
+    outfile << "Assignment 4 - Resizing the Stack\nCMPS 3013\nLanden Jones\n\n";
+    outfile << "Max Stack Size: " << stack.Max() << '\n';
+    outfile << "End Stack Size: " << stack.PrintSize() << '\n';
+    outfile << "Stack Resized: " << stack.NumResized() << "\n\n";
+    outfile << "######################################################################\n";
+    return 0;
+}
 #include <iostream>
 #include <fstream>
 using namespace std;
