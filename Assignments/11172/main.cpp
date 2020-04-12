0cd3324d5955a254341872da4ea0e6632c8a6a6d
Landen Jones <59664899+LandenSJones@users.noreply.github.com>
2020-04-12 16:39:58 -0500

    Update main.cpp

--- Assignments/11172/main.cpp
+++ Assignments/11172/main.cpp
@@ -1,1 +1,15 @@
-main
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	int x, count = 1;
+	cin >> x;
+	while (x != 0)
+	{
+		cout << "Case " << count++ << ": " <<  x / 2 << endl;
+		cin >> x;
+	}
+	return 0;
+}
